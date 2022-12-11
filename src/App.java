e610185165829b8aff65f08ae5020492e1cc5f56
e225710 <e225710@ie.u-ryukyu.ac.jp>
2022-12-12 00:00:34 +0900

    2st push

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException error was occured!");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
 
