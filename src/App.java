f21418a9b0a6a4cf2e8da5ff6d6df73e8ac06ce2
e225710 <e225710@ie.u-ryukyu.ac.jp>
2022-12-11 23:41:49 +0900

    2st push

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException error was occured!");
+        }
     }
 }
